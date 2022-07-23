3253d3b806bb2cc658a84961e925a0b7e2791f6b
Marvin Bentinganan <marvbentinganan@gmail.com>
2022-07-23 11:59:21 +0800

    upgrade to laravel 9

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::get('/', function () {
-    return 'never gonna give you up';
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
